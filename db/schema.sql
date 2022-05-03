7ca99352df7786b487eb3b1d3f77eaa9d72440cf
callanhunter <callan57hunter@gmail.com>
2022-05-02 18:18:55 -0700

    added the table content to seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,58 @@
-DROP DATABASE IF EXISTS books_db;
-CREATE DATABASE books_db;
+-- department
+--   id: INT PRIMARY KEY
 
-USE books_db;
+--   name: VARCHAR(30) to hold department name
 
-CREATE TABLE favorite_books (
+-- role
+--   id: INT PRIMARY KEY
+
+--   title: VARCHAR(30) to hold role title
+
+--   salary: DECIMAL to hold role salary
+
+--   department_id: INT to hold reference to department role belongs to
+
+-- employee
+--   id: INT PRIMARY KEY
+
+--   first_name: VARCHAR(30) to hold employee first name
+
+--   last_name: VARCHAR(30) to hold employee last name
+
+--   role_id: INT to hold reference to employee role
+
+--   manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+
+
+DROP DATABASE IF EXISTS management_db;
+CREATE DATABASE management_db;
+
+USE management_db;
+
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  book_name VARCHAR(30) NOT NULL
+  name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) 
+  REFERENCES role(id),
+  FOREIGN KEY (manager_id) 
+  REFERENCES employee(id)
+);
+
